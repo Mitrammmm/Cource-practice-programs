41383ce7311011c2ed6083c0716ce67b2eedc6e4
Mitrammmm <mitrasinghimp@gmail.com>
2025-08-29 13:31:23 +0530

    29th aug 25

---
+++ 60.Graph/Dijkstra Algorithm.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// typedef pair<int, int> pii; // {distance, node}
+
+void dijkstra(int start, int V, vector<vector<pair<int,int>>>& adj) {
+    vector<int> dist(V, INT_MAX);   // distance array
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+    dist[start] = 0;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (d > dist[u]) continue; // skip if outdated
+
+        for (auto edge : adj[u]) {
+            int v = edge.first;
+            int w = edge.second;
+
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    // Print shortest distances
+    cout << "Shortest distances from node " << start << ":\n";
+    for (int i = 0; i < V; i++) {
+        if (dist[i] == INT_MAX) cout << i << " : INF\n";
+        else cout << i << " : " << dist[i] << "\n";
+    }
+}
+
+int main() {
+    int V = 5; // number of vertices
+    vector<vector<pair<int,int>>> adj(V);
+
+    // Example graph (undirected, weighted)
+    adj[0].push_back({1, 2});
+    adj[1].push_back({0, 2});
+
+    adj[0].push_back({2, 4});
+    adj[2].push_back({0, 4});
+
+    adj[1].push_back({2, 1});
+    adj[2].push_back({1, 1});
+
+    adj[1].push_back({3, 7});
+    adj[3].push_back({1, 7});
+
+    adj[2].push_back({4, 3});
+    adj[4].push_back({2, 3});
+
+    int start = 0;
+    dijkstra(start, V, adj);
+
+    return 0;
+}
---
+++ 60.Graph/Prims Algorithm.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void primMST(int V, vector<vector<pair<int,int>>>& adj) {
+    vector<int> key(V, INT_MAX);      // Minimum edge weight to connect node
+    vector<int> parent(V, -1);        // To store MST structure
+    vector<bool> mst(V, false);       // True if vertex is included in MST
+
+    // Min-heap {key, vertex}
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+    key[0] = 0;   // Start from node 0
+    pq.push({0, 0});
+
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+
+        if(mst[u]) continue;   // already included
+        mst[u] = true;
+
+        // Explore neighbors
+        for(auto [v, wt] : adj[u]){
+            if(!mst[v] && wt < key[v]){
+                key[v] = wt;
+                parent[v] = u;
+                pq.push({key[v], v});
+            }
+        }
+    }
+
+    // Print MST edges
+    cout << "Edges in MST:\n";
+    for(int i = 1; i < V; i++){
+        cout << parent[i] << " - " << i << "  (weight: " << key[i] << ")\n";
+    }
+}
+
+
+int main(){
+    int V = 5; // Number of vertices
+    vector<vector<pair<int,int>>> adj(V);
+
+    // Example graph
+    auto addEdge = [&](int u, int v, int w){
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    };
+
+    addEdge(0, 1, 2);
+    addEdge(0, 3, 6);
+    addEdge(1, 2, 3);
+    addEdge(1, 3, 8);
+    addEdge(1, 4, 5);
+    addEdge(2, 4, 7);
+    addEdge(3, 4, 9);
+
+    primMST(V, adj);
+    return 0;
+}
